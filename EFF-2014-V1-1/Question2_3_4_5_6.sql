542edfd171f8c37d7707e114db11a27722e8ef91
Hamza Maach <hamzamaach56@gamil.com>
2022-11-06 22:07:01 +0100

    Question 3 et 6

--- EFF-2014-V1-1/Question2_3_4_5_6.sql
+++ EFF-2014-V1-1/Question2_3_4_5_6.sql
@@ -2,6 +2,15 @@
 ---------------------------------------------------------
 --
 
+
+--
+---------------------------------------------------------
+
+
+
+-- Qustion 3
+---------------------------------------------------------
+--
 DROP FUNCTION IF EXISTS nombrePoint;
 DELIMITER &&
 CREATE FUNCTION nombrePoint(id int)
@@ -13,43 +22,14 @@ DECLARE nombreEnfants int;
 DECLARE nombrePoint int;
 
 SET dateAffectation = (SELECT dateAffLycee FROM professeur WHERE idProfesseur = id);
-SET nombreAnnees = year(CURDATE())-year(dateAffectation);
 SET nombreEnfants = (SELECT nEnfants FROM professeur WHERE idProfesseur = id);
+SET nombreAnnees = year(CURDATE())-year(dateAffectation);
 SET nombrePoint = nombreEnfants + nombreAnnees * 2;
 
 RETURN nombrePoint;
 END && 
 
 
--- DROP FUNCTION IF EXISTS
---     nombrePoint;
--- DELIMITER
---     &&
--- CREATE FUNCTION nombrePoint(id INT) RETURNS INT BEGIN
---     DECLARE
---         nombrePoint INT ;
---     DECLARE dateAffectation date;
---     SET dateAffectation = (
---         SELECT dateAffLycee FROM professeur 
---         WHERE idProfesseur = id);
---     SET nombrePoint = (
---         SELECT nEnfants
---         FROM professeur WHERE idProfesseur = id ) +
---         (YEAR(CURDATE() - YEAR(dateAffectation)) * 2 ; 
---         RETURN nombrePoint ;
---     END &&
-
-
-
-
---
----------------------------------------------------------
-
-
-
--- Qustion 3
----------------------------------------------------------
---
 --
 ---------------------------------------------------------
 
@@ -61,7 +41,19 @@ END &&
 --
 ---------------------------------------------------------
 
+DROP PROCEDURE IF EXISTS AffLycee;
+DELIMITER &&
+CREATE PROCEDURE AffLycee(id int)
+BEGIN 
 
+
+
+SELECT Ville FROM Lycee WHERE idLycee = id;
+
+
+
+
+END
 
 -- Qustion 5
 ---------------------------------------------------------
@@ -74,5 +66,19 @@ END &&
 -- Qustion 6
 ---------------------------------------------------------
 --
+
+
+DROP TRIGGER IF EXISTS Celeb;
+DELIMITER &&
+CREATE TRIGGER `Celeb` 
+BEFORE 
+INSERT ON professeur
+FOR EACH ROW 
+    IF NEW.EtatCivil = "celebataire" AND NEW.NEnfants > 0 
+    THEN SIGNAL SQLSTATE '50001' 
+    SET MESSAGE_TEXT ="Le professeur qu'est celebataire ne peut pas avoir des enfants" ;
+	END IF;&&
+
+
 --
 ---------------------------------------------------------

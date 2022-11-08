a256d25771d339af4383331ec9016efb1069ec92
Hamza Maach <hamzamaach56@gamil.com>
2022-11-08 23:09:38 +0100

    Question 4 et 5

--- EFF-2014-V1-1/Question2_3_4_5_6.sql
+++ EFF-2014-V1-1/Question2_3_4_5_6.sql
@@ -41,23 +41,65 @@ END &&
 --
 ---------------------------------------------------------
 
-DROP PROCEDURE IF EXISTS AffLycee;
+DROP PROCEDURE IF EXISTS AffLyceePlusDemende;
 DELIMITER &&
-CREATE PROCEDURE AffLycee(id int)
+CREATE PROCEDURE AffLyceePlusDemende(IN `idRegionV` INT, OUT `LyceeMax` VARCHAR(20))
 BEGIN 
-
-
-
-SELECT Ville FROM Lycee WHERE idLycee = id;
-
-
-
-
-END
+    DECLARE idLyceeMax int ;
+    SET idLyceeMax =(
+                    SELECT dd.idLycee 
+                    FROM Detail_Demande dd 
+                    WHERE idLycee IN(
+                        SELECT l.idLycee 
+                        FROM Lycee l 
+                        INNER JOIN Academie a
+                        ON a.idAcademie = l.idAcademie
+                        INNER JOIN Region r 
+                        ON r.idRegion = a.idRegion
+                        WHERE r.idRegion = idRegionV
+                    )
+                    GROUP BY idLycee
+                    ORDER BY COUNT(idLycee)
+                    DESC LIMIT 1
+                );
+    SET LyceeMax = (
+                    SELECT nomLycee 
+                    FROM Lycee 
+                    WHERE idLycee = idLyceeMax
+                );
+END &&
 
 -- Qustion 5
 ---------------------------------------------------------
 --
+
+
+DROP PROCEDURE IF EXISTS AffDemendeDiffRegion;
+DELIMITER &&
+CREATE PROCEDURE AffDemendeDiffRegion()
+BEGIN 
+    SET professeur =(
+                        SELECT
+                    idProfesseur
+                FROM
+                    professeur
+                WHERE
+                    idProfesseur IN(
+                    SELECT
+                        l.idLycee
+                    FROM
+                        professeur p
+                    INNER JOIN Lycee l ON
+                        p.idLycee = l.idLycee
+                    INNER JOIN Academie a ON
+                        a.idAcademie = l.idAcademie
+                    INNER JOIN Region r ON
+                        r.idRegion = a.idRegion
+                );
+        
+END &&
+
+
 --
 ---------------------------------------------------------
 
@@ -74,7 +116,8 @@ CREATE TRIGGER `Celeb`
 BEFORE 
 INSERT ON professeur
 FOR EACH ROW 
-    IF NEW.EtatCivil = "celebataire" AND NEW.NEnfants > 0 
+    IF NEW.EtatCivil = "celebataire" 
+    AND NEW.NEnfants > 0 
     THEN SIGNAL SQLSTATE '50001' 
     SET MESSAGE_TEXT ="Le professeur qu'est celebataire ne peut pas avoir des enfants" ;
 	END IF;&&
